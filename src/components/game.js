d1242581ab9fa69b81c3520ae5705961df9e5719
Delvio <elvioseverino@gmail.com>
2022-07-27 11:12:10 -0400

    created game component

---
+++ src/components/game.js
@@ -0,0 +1,13 @@
+import Board from "./components/board";
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+export default Game;
